85160fa1720bd5cb6f5820faafb5f51817750d98
Nik Bornemeier <NikolausRB@gmail.com>
2017-10-21 10:07:57 -0600

    install and hook up router

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import MovieCatalog from '../MovieCatalog/MovieCatalog.js';
+import { Route } from 'react-router';
+import { withRouter } from 'react-router-dom';
+import Modal from '../Modal/Modal';
+import SignIn from '../SignIn/SignIn';
+import SignUp from '../SignUp/SignUp';
 
-export default class App extends Component {
+
+
+
+class App extends Component {
   constructor() {
     super();
   }
 
   render() {
     return (
+
       <div className='grid-app'>
-        <MovieCatalog />
-      </div>
+
+        <Route exact path='/signin'
+          render={() =>
+            <SignIn />}
+        />
+        <Route exact path='/signup'
+          render={() =>
+            <SignUp/>}
+        />
+        <Route path='/'
+          render={() =>
+            <MovieCatalog />}
+        />
+       
+        </div>
+
     )
   }
 }
+
+export default withRouter(App);
